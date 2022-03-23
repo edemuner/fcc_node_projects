3c330179278d5a62386dcf2a09c315228422c7af
edemuner <eduardood15@hotmail.com>
2022-03-23 14:57:57 -0300

    started to make the timeConverter, that receives the request url parameters and checks if it has only numbers

--- timestamp_microservice/controller.js
+++ timestamp_microservice/controller.js
@@ -1,5 +1,16 @@
 
+function timeConverter(req, res){
+    const {time} = req.params
+    if(time.match(/^[0-9]+$/) != null){
+        res.send('only numbers')
+    } else {
+        res.send('not only numbers')
+    }
+}
+
 module.exports = app => {
 
-    
+    app.get('/', (req, res) => res.send('You came to your destiny'))
+
+    app.get('/api/:time', timeConverter)
 }
