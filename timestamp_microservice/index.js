eb4e564675691cceed39af5fa1bf0dc7c4982e10
edemuner <eduardood15@hotmail.com>
2022-03-23 14:29:05 -0300

    prepared the server listening, connected to the controller (not yet implemented)

---
+++ timestamp_microservice/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const bodyparser = require('body-parser')
+const controller = require('./controller')
+
+const app = express()
+
+app.use(bodyparser.json())
+
+controller(app)
+
+const port = 3000
+app.listen(port, () => console.log('App listening on port 3000'))
+
+module.exports = app
