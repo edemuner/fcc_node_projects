c5e7cebed924415ee43f524d6511603e4f9420e6
edemuner <eduardood15@hotmail.com>
2022-03-28 14:56:05 -0300

    changed try/catch to a check if date isNaN

--- controller.js
+++ controller.js
@@ -3,25 +3,26 @@ function timeConverter(req, res){
     const {time} = req.params
     // unix pattern has only numbers
     if(time.match(/^[0-9]+$/) != null){
-        try{
             const date = new Date(Number(time))
-            res.status(200).json({
-                unix:date.getTime(),
-                utc:date.toUTCString()
-            })
-        } catch(error) {
-            res.status(400).json({error:"Invalid Date"})
-        }
+            if (isNaN(date)){
+                console.log(date)
+                res.status(400).json({error:"Invalid Date"})
+            } else {
+                res.status(200).json({
+                    unix:date.getTime(),
+                    utc:date.toUTCString()
+                })
+            }
     } else {
-        try{
             const date = new Date(time)
-            res.status(200).json({
-                unix:date.getTime(),
-                utc:date.toUTCString()
-            })
-        } catch {
-            res.status(400).json({error:"Invalid Date"})
-        }
+            if (isNaN(date)){
+                res.status(400).json({error:"Invalid Date"})
+            } else {
+                res.status(200).json({
+                    unix:date.getTime(),
+                    utc:date.toUTCString()
+                })
+            }
     }
 }
 
